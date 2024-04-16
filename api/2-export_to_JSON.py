a44e9a53f04b9f2e5216b72065ed7a617d17c899
rivero04 <7710@holbertonstudents.com>
2024-04-16 06:13:21 -0700

    Task 2

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""API PROJECT"""
+
+import requests
+import sys
+import json
+
+def get_employee_task_progress(user_id):
+    """
+    Exports an employee's task progress to a JSON file.
+    """
+
+    user = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    todos = f"https://jsonplaceholder.typicode.com/todos/?userId={user_id}"
+
+    user= requests.get(user).json()
+    todo = requests.get(todos).json()
+
+    tasks = []
+    for task in todo:
+        tasks.append({
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": user.get('username')
+        })
+
+    data = {user_id: tasks}
+
+    with open(f"{user_id}.json", "w") as json_file:
+        json.dump(data, json_file)
+        
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        get_employee_task_progress(sys.argv[1])
